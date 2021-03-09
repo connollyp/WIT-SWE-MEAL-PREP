28af16920c4353844a138eea4e683f5c84ccc8bf
Patrick Connolly <pat580000@gmail.com>
2021-03-09 10:44:57 -0500

    Created new folders & added basic config form file

---
+++ mealprep/src/Views/ConfigurationPage.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class ConfigPage extends React.Component {
+  constructor(props){
+      super(props)
+  }
+
+  validate = (data) => {
+  }
+
+
+  handleSubmit = (data) => {
+  };
+
+  render(){
+    
+    return(
+        <div>
+        Test
+        </div>) 
+  }
+
+}
+export default ConfigPage;
