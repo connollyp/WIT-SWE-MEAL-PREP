738e40bfb3c6496cfe4f051643d28e02515bf45b
Patrick Connolly <pat580000@gmail.com>
2021-03-11 13:22:13 -0500

    Added log in page with basic data routing

--- mealprep/src/Components/Main.jsx
+++ mealprep/src/Components/Main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ConfigPage from './ConfigurationPage.jsx';
+import LogInPage from './LogInPage.jsx'
 
 class Main extends React.Component{
 
@@ -7,29 +8,60 @@ class Main extends React.Component{
         super(props);
 
         this.state = {
-            configured: false
+            configured: false,
+            loggedIn: false,
+            username: '',
+            password: ''
         }
     }
 
     handleSubmit = () => {
         this.setState({
-            configured: true
+            configured: true,
         })
+    }
+
+    handleLogIn = () => {
+
+        this.setState({
+            username: document.getElementById("username").value,
+            password: document.getElementById("password").value
+        })
+        
+
+        //Should send to database to verify that its working
+        //Should make a log in controller for this
+
+        this.setState({
+            loggedIn: true
+        })
+
     }
     
     render() { 
 
-        if(this.state.configured){
+        if(this.state.loggedIn){
+            if(this.state.configured){
 
-            return(<div><h1>Configured</h1></div>)
+                console.log(this.state);
 
+                return(<div><h1>Configured</h1></div>)
+    
+            }else{
+                return(
+                    <ConfigPage 
+                    handleSubmit={this.handleSubmit} 
+                    />
+                    )
+            }
         }else{
             return(
-                <ConfigPage 
-                handleSubmit={this.handleSubmit} 
-                />
-                )
+            <LogInPage 
+            handleLogIn={this.handleLogIn}
+            />
+            )
         }
+
     }
 }
 
