bdac9909f0d4ec2e6b807cffa9f09326f9456d11
Patrick Connolly <pat580000@gmail.com>
2021-04-03 11:03:35 -0400

    Fixed env file

--- mealprep/public/env.js
+++ mealprep/public/env.js
@@ -1,5 +1,5 @@
 window.env = {
-  "DATABASE_HOST": "gainsday.c81mmfyf7fbi.us-east-2.rds.amazonaws.com",
-  "DATABASE_USER": "admin",
-  "DATABASE_PASSWORD": "Gainsday1"
+    "DATABASE_HOST": "gainsday.c81mmfyf7fbi.us-east-2.rds.amazonaws.com",
+    "DATABASE_USER": "admin",
+    "DATABASE_PASSWORD": "Gainsday1"
 };
