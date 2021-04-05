338b13bcfcfcbe05be74c6a5d29c56340887302c
Patrick Connolly <pat580000@gmail.com>
2021-04-05 12:19:36 -0400

    Ui stylings and routing

--- mealprep/src/Views/MainPage.jsx
+++ mealprep/src/Views/MainPage.jsx
@@ -1,21 +1,79 @@
 import React from 'react';
+import '../Stylings/MainStylings.css'
 
 class MainPage extends React.Component{
   constructor(props){
       super(props)
 
       this.state = {
-        
+        renderSearchPage: false,
+        searchQuery: "",
+        username: this.props.username,
+        constraints: this.props.constraints
       }
   }
 
-  render(){
-    return(
-      <div>
+  handleInputChange = () => {
+    this.setState({
+      searchQuery: this.search.value
+    })
+  }
 
-      </div>
-    )
+  handleSearchSubmit = () => {
+    this.setState({
+      renderSearchPage: true
+    })
+  }
+
+  render(){
+
+    if(!this.state.renderSearchPage){
+      return(
+        <div>
+          <form onSubmit={this.handleSearchSubmit}>
+            <input
+              placeholder="Look up food..."
+              ref={input => this.search = input}
+              onChange={this.handleInputChange}
+              className="search"
+            />
+            <p className="search">{this.state.searchQuery}</p>
+          </form>
+
+          <div>
+
+            <table>
+              <tbody>
+                <tr>
+                  <th>Breakfast</th>
+                  <th>Morning Snack</th>
+                  <th>Lunch</th>
+                  <th>Afternoon Snack</th>
+                  <th>Dinner</th>
+                  <th>Dessert</th>
+                </tr>
+                <tr>
+                  <td>Scrambled Eggs</td>
+                  <td>Apple</td>
+                  <td>Turkey Club</td>
+                  <td>Yogurt and Chia Seeds</td>
+                  <td>Chop Suey</td>
+                  <td>Protein Shake</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      )
+    }else{
+      return(
+        <div>
+          {this.state.searchQuery}
+        </div>
+      )
+    }
+
   }
 }
 
-export default LogInPage; 
+export default MainPage; 
