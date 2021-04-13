44823ab1381cbde67531f61b19ec68c5f19da5a4
Patrick Connolly <pat580000@gmail.com>
2021-04-13 14:47:44 -0400

    finally fixed it

--- mealprep/src/App.js
+++ mealprep/src/App.js
@@ -1,27 +1,13 @@
 import React, { useState } from 'react';
 
 // import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import MainController from './Controllers/MainController.jsx'
-import AccountStatusController from './Controllers/AccountStatusController.jsx';
-import Footer from './Components/Footer.jsx'
-import Header from './Components/Header.jsx'
+import AppController from './Controllers/AppController.jsx'
 
-import './Stylings/AppStylings.css'
+// import './Stylings/AppStylings.css'
 
 function App() {
-    const [loggedIn, isLoggedIn] = useState();
-
-    if (!loggedIn) {
-        return <AccountStatusController isLoggedIn = { isLoggedIn }
-        />
-    }
-
     return (
-        <div className="mainAppWrapper">
-            <Header/>
-            <MainController/>
-            <Footer/>
-        </div>
+        <AppController/>
     )
 }
 
